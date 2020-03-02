034af2496a1e16e7fe8797eb9345f72b626b4248
sai <vsreenathreddy1@gmail.com>
2020-03-02 01:12:16 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,21 @@
 from flask import Flask,render_template,request
+import sqlite3 as sql
+import pandas as pd
+import numpy as np
+application = app = Flask(__name__)
+import os
+import time
+import redis
+import pickle as pickle
+import random
+from sklearn.cluster import KMeans
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib import pyplot as mpld3
+from sklearn.cluster import KMeans
+from scipy.spatial import distance
+
+
 app = Flask(__name__)
 
 con = sql.connect("database.db")
