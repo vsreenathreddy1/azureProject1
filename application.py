1fececd99c172aa34f2d6d62f0cecd7c01b69c80
vsreenathreddy1 <vsreenathreddy1@gmail.com>
2020-03-01 23:51:49 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('home.html')
+
+if __name__ == '__main__':
+   app.run()
