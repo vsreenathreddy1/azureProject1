36ffab3e9f82242ef89ca873a910c8fb399cc5f8
vsreenathreddy1 <vsreenathreddy1@gmail.com>
2020-03-02 00:53:02 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request
 app = Flask(__name__)
 
+con = sql.connect("database.db")
+rd = redis.StrictRedis(host='Saii.redis.cache.windows.net', port=6380, db=0,password='7Z6Az0CdHRKCxGb3+a+XLxU52cc9xMIZPkXjJAfAn5U=',ssl=True)
+
 @app.route("/")
 def hello():
     return render_template('home.html')
